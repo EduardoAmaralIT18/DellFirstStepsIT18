86763c28c660f4f679bed5f4028975841172c86f
JordanaSantosIT17 <jordana.santos@dellteam.com>
2023-01-11 16:13:34 -0300

    Delete HomeController e Add ProgramService, IProgramService

---
+++ ConnectDellBack/Services/IProgramService.cs
@@ -0,0 +1,10 @@
+﻿using ConnectDellBack.Models;
+
+namespace ConnectDellBack.Services
+{
+    public interface IProgramService
+    {
+        public Task<IEnumerable<ProgramModel>> GetPrograms(int idUser, string role);
+        public Task<IEnumerable<ProgramModel>> GetMyPrograms(int idUser, string role);
+    }
+}
