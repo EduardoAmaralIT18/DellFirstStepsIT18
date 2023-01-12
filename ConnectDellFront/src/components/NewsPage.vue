a7a841de364a809f23d982f3c2323f75becde790
Larissa_RosaIT17 <larissa.rosa@dellteam.com>
2023-01-12 16:12:10 -0300

    NewsPage:
    +button seemore news NewsPage.vue
    +exemplo de imagem estatica na news

--- ConnectDellFront/src/components/NewsPage.vue
+++ ConnectDellFront/src/components/NewsPage.vue
@@ -8,28 +8,32 @@
                 </button>
             </div>
         </div>
-        <div data-dds="pagination" id="unique-id">
-            <div v-if="news != null" class="dds__col-12 dds__col--md-6 dds__col--lg-4 dds__mb-3">
-                <div v-for="item in newsShown" :key="item.id" class="dds__card" id="card-news">
-                    <div v-if="item.image" class="dds__card__media">
-                        <img src="item.image">
+        <div v-if="news != null" class="dds__col-12 dds__col--md-6 dds__col--lg-4 dds__mb-3">
+            <div v-for="item in newsShown" :key="item.id" class="dds__card" id="card-news">
+                <div class="dds__card__media">
+                    <!-- <div v-if="item.image != null" class="dds__card__media"></div> -->
+                    <img src="https://super.abril.com.br/wp-content/uploads/2017/12/a-verdadeira-histc3b3ria-de-natal.png?w=1024" />
+                </div>
+                <div class="dds__card__content">
+                    <div class="dds__card__header">
+                        <span class="dds__card__header__text">
+                            <h5 class="dds__card__header__title"><b>{{ item.title }}</b></h5>
+                            <p><i>Posted by {{ item.author }} from {{ item.program }}</i></p>
+                        </span>
                     </div>
-                    <div class="dds__card__content">
-                        <div class="dds__card__header">
-                            <span class="dds__card__header__text">
-                                <h5 class="dds__card__header__title"><b>{{ item.title }}</b></h5>
-                                <p><i>Posted by {{ item.author }} from {{ item.program }}</i></p>
-                            </span>
-                        </div>
-                        <div class="dds__card__body">
-                            {{ item.text }}
-                        </div>
-                        <div class="dds__card__footer">
-                            <p>{{ item.date }}</p>
-                        </div>
+                    <div class="dds__card__body">
+                        {{ item.text }}
+                    </div>
+                    <div class="dds__card__footer">
+                        <p>{{ item.date }}</p>
                     </div>
                 </div>
             </div>
+        </div>
+        <div v-if="countNews != totalNews">
+            <button class="dds__button dds__button--primary dds__button--lg" type="button" @click="moreNews">
+                See More
+            </button>
         </div>
     </div>
 </template>
@@ -41,9 +45,10 @@ type News = {
 }[];
 
 interface Data {
-    news: null | News ,
+    news: null | News,
     role: number,
-    newsShown: null | News ,
+    newsShown: null | News,
+    totalNews: number | undefined,
     countNews: number
 }
 
@@ -53,6 +58,7 @@ export default defineComponent({
             news: null,
             role: 5,
             newsShown: [],
+            totalNews: 0,
             countNews: 0
         };
     },
@@ -63,7 +69,7 @@ export default defineComponent({
     },
     watch: {
         // call again the method if the route changes
-        '$route': 'fetchData'
+        '$route': 'fetchData',
     },
     methods: {
         fetchData(): void {
@@ -81,23 +87,28 @@ export default defineComponent({
                         return;
                     } else if (response.status == 200) {
                         this.news = response.data;
+                        this.totalNews = this.news?.length;
                         this.moreNews();
                         return;
                     } else {
                         console.log(response.status);
                         return;
                     }
-                    
+
                 });
         },
         moreNews(): void {
-            this.news?.every(element => {
-                 console.log(this.newsShown?.push(element));
-                this.countNews = this.countNews + 1;
-                if (this.countNews % 10 == 0) {
-                    return false;
-                }
-            });
+            do {
+                this.news?.every(element => {
+                    this.newsShown?.push(element);
+                    this.news?.shift();
+                    console.log(element);
+                    this.countNews = this.countNews + 1;
+                });
+            } while ((this.countNews % 10 != 0) && (this.countNews != this.totalNews));
+            console.log(this.totalNews);
+            console.log(this.news?.length);
+            console.log(this.countNews);
         }
     },
 });
@@ -118,6 +129,10 @@ export default defineComponent({
 
 i {
     margin-right: 5px;
+}
+
+.dds__card__footer p {
+    color: #7E7E7E;
 }
 
 p {
