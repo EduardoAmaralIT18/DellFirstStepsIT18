4f0d5fc51e5aac537f95d62332ee42c1893b7c90
LeandraTorbesIT17 <leandra.torbes@dellteam.com>
2023-01-12 14:45:39 -0300

    Ajuste Service

--- ConnectDellBack/Services/IUserService.cs
+++ ConnectDellBack/Services/IUserService.cs
@@ -1,33 +1,9 @@
-using Microsoft.EntityFrameworkCore;
-using ConnectDellBack.Models;
+using DellFirstSteps.Models;
+using System.Threading.Tasks;
 
-namespace ConnectDellBack.Services;
-
-    public interface IUserService
-    {
-
-    Task<IEnumerable<ProgramModel>> Get();
-
-    //Task<ProgramModel> Get(int Id);
-
-
-    }
-
-
-
-//criar usuário
-/*public string createUser (UserModel user);
-
-//ligando usuário ao programa
-public string assignProgram (int idUser, int idProgram);
-
-public string createProgram(ProgramModel program);
-
-public string assignEdition(int idUser, int idEdition);
-
-public string createEdition(EditionModel edition);
-
-
-//public changeRole(); //?????????????????????????
-
-public IEnumerable<UserModel> listUser(); */
+namespace DellFirstSteps.Services;
+public interface IUserService
+ {
+    public Task<IEnumerable<UserModel>> GetUser();
+ 
+}
