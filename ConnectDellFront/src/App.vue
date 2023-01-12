718375194b70d7a6f8b3e294adcae3802eb24043
LauraTeixeiraIT17 <Laura.TeixeiraNunesDaSilveira@dellteam.com>
2023-01-12 15:19:44 -0300

    alteração na formatação

--- ConnectDellFront/src/App.vue
+++ ConnectDellFront/src/App.vue
@@ -4,31 +4,31 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import NavBar from './components/NavBar.vue';
-import SideBar from './components/SideBar.vue';
-export default defineComponent({
-  name: 'App',
-  components: {
-    NavBar,
-    SideBar
-  },
-  data() {
-    return {}
-  },
-  created() {
-      this.$cookies.set("NAME", "Elise"),
-      this.$cookies.set("ROLE", "Intern")
-  }
-});
+  import { defineComponent } from 'vue';
+  import NavBar from './components/NavBar.vue';
+  import SideBar from './components/SideBar.vue';
+  export default defineComponent({
+    name: 'App',
+    components: {
+      NavBar,
+      SideBar
+    },
+    data() {
+      return {}
+    },
+    created() {
+        this.$cookies.set("NAME", "Elise"),
+        this.$cookies.set("ROLE", "Intern")
+    }
+  });
 </script>
 
 <style>
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
