0a8db14386b8c3e57ae0be32d8350c10fd1e5bc8
Larissa_RosaIT17 <larissa.rosa@dellteam.com>
2023-01-13 14:47:47 -0300

    ajuste navbar

--- ConnectDellFront/src/views/NewsView.vue
+++ ConnectDellFront/src/views/NewsView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import NewsPage from '../components/NewsPage.vue'
+import NewsPage from '../components/NewsPage.vue';
+import NavBar from './components/NavBar.vue';
+import SideBar from './components/SideBar.vue';
 import { RouterView } from 'vue-router';
 </script>
 <template>
   <main>
+    <NavBar></NavBar>
+    <SideBar view="home"></SideBar>
     <NewsPage />
     <RouterView />
   </main>
