998c0f09e3f79f6c2feb9c0b06b77d3f18a97de3
Alexya Silva <122369580+AlexyaOliveiraIT17@users.noreply.github.com>
2023-01-16 11:46:07 -0300

    Não delete as alteração no ProgramService

--- ConnectDellBack/Services/ProgramService.cs
+++ ConnectDellBack/Services/ProgramService.cs
@@ -1,25 +1,47 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ConnectDellBack.Models;
 using ConnectDellBack.DTOs;
-
 namespace ConnectDellBack.Services;
-
 public class ProgramService : IProgramService
 {
     private readonly ApplicationContext _dbContext;
-
     public ProgramService(ApplicationContext dbContext)
     {
         _dbContext = dbContext;
     }
 
+    public async Task<ProgramDTO> GetPrograms(int idUser, int role)
     public async Task<ProgramDTO> GetPrograms(int idUser, string role)
     {
+        var user = _dbContext.users.FirstOrDefault(u => u.id == idUser);
+@AlexyaOliveiraIT17 AlexyaOliveiraIT17 Pending
+Não deve ser pagado
+
+@AlexyaOliveiraIT17	Reply...
+        var programs = await _dbContext.programs.OrderBy(p => p.startDate).ToListAsync();
+
+        var myPrograms = new List<ProgramModel>();
+        switch (role)
+        {
+            case 0: //Admin
+                myPrograms = programs; //TODO: filter
+                break;
+            case 1: //Intern
+                myPrograms = programs; //TODO: filter
+                break;
+            default: //Other
+                myPrograms = programs; //TODO: filter
+                break;
+        }
+
         var programDTO = new ProgramDTO
         {
+            Programs = programs.Except(myPrograms).ToList(),
+            MyPrograms = myPrograms
             Programs = await _dbContext.programs.OrderBy(p => p.startDate).ToListAsync(),
             MyPrograms = await _dbContext.programs.OrderBy(p => p.startDate).ToListAsync()
         };
+
         return programDTO;
     }
 
