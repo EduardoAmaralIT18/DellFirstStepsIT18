09e4e5449a4bec55ca920df75e1d558898955a90
Larissa_RosaIT17 <larissa.rosa@dellteam.com>
2023-01-18 13:25:43 -0300

    alteração das lists nas models

--- ConnectDellBack/Models/ProgramModel.cs
+++ ConnectDellBack/Models/ProgramModel.cs
@@ -19,10 +19,10 @@ public class ProgramModel{
     [StringLength(500, MinimumLength = 10, ErrorMessage = "The program's description must be at most 500 characters.")]
     public string description {get;set;} = null!;
 
-    public List<EditionModel>? editions {get;set;}
-    public List<NewsModel>? news {get;set;}
+    public List<EditionModel>? editions {get;set;} = new List<EditionModel>();
+    public List<NewsModel>? news {get;set;} = new List<NewsModel>();
     [Required]
-    public List<UserModel> owners {get;set;} = null!;
+    public List<UserModel> owners {get;set;} = new List<UserModel>();
 
-    public List<OwnershipModel>? ownerships {get;set;}
+    public List<OwnershipModel>? ownerships {get;set;} = new List<OwnershipModel>();
 }
--- ConnectDellBack/Models/UserModel.cs
+++ ConnectDellBack/Models/UserModel.cs
@@ -25,12 +25,12 @@ public class UserModel{
     // intership log (text to fill with information about which activities were performed.)
     public String notes { get; set; } =  null!; 
 
-    public List<EditionModel>? listEditions {get;set;}
+    public List<EditionModel>? listEditions {get;set;} = new List<EditionModel>();
     public EditionModel? editionIntern {get;set;}
-    public List<OwnershipModel>? ownerships {get;set;}
-    public List<ProgramModel>? ProgramsAdmins {get;set;}
-    public List<MembershipModel>? memberships {get;set;}
-    public List<NewsModel>? listNews {get; set;}
+    public List<OwnershipModel>? ownerships {get;set;} = new List<OwnershipModel>();
+    public List<ProgramModel>? ProgramsAdmins {get;set;} = new List<ProgramModel>();
+    public List<MembershipModel>? memberships {get;set;} = new List<MembershipModel>();
+    public List<NewsModel>? listNews {get; set;} = new List<NewsModel>();
 
     //Comentando Projeto 
 
