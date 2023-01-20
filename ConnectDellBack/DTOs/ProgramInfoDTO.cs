7f8ca1926f972e4ef4075ceb2c9191edff0ee14f
KristenArguelloIT17 <Kristen.Arguello@dellteam.com>
2023-01-20 12:34:55 -0300

    Get do program info funcionando

---
+++ ConnectDellBack/DTOs/ProgramInfoDTO.cs
@@ -0,0 +1,40 @@
+using ConnectDellBack.Models;
+
+namespace ConnectDellBack.DTOs;
+
+public class ProgramInfoDTO
+{
+    public string name { get; set; } = null!;
+    public DateTime startDate { get; set; }
+    public DateTime? endDate { get; set; }
+    public string description { get; set; }
+    public List<EditionModel>? editions { get; set; } = new List<EditionModel>();
+    public List<UserModel> owners { get; set; } = new List<UserModel>();
+
+    public static ProgramInfoDTO convertModel2DTO(ProgramModel program)
+    {
+        ProgramInfoDTO aux = new ProgramInfoDTO();
+        aux.name = program.name;
+        aux.startDate = program.startDate;
+        if (program.endDate != null)
+        {
+            aux.endDate = program.endDate;
+        }
+        aux.description = program.description;
+        foreach (var item in program.editions)
+        {
+            aux.editions.Add(new EditionModel()
+            {
+                name = item.name,
+                description = item.description
+            });
+        }
+        foreach (var item in program.owners) {
+            aux.owners.Add(new UserModel() {
+                name = item.name,
+                id = item.id
+            });
+        }
+        return aux;
+    }
+}
