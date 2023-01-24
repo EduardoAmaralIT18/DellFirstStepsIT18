23c2765a06e78bfd5be0c7a942f6cb430ec611cf
Alexya Silva <122369580+AlexyaOliveiraIT17@users.noreply.github.com>
2023-01-24 16:48:39 -0300

    Update NewsModel.cs
    fix nulls

--- ConnectDellBack/Models/NewsModel.cs
+++ ConnectDellBack/Models/NewsModel.cs
@@ -8,16 +8,16 @@ public class NewsModel {
     
     [Required]
     [StringLength(100, MinimumLength = 10, ErrorMessage = "The length of the news title should be at least 10 characters.")]
-    public string title {get;set;}
+    public string title {get;set;}  = null!;
 
     [Required]
     [StringLength(5000, MinimumLength = 10, ErrorMessage = "The length of the news text should be at least 10 characters.")]
-    public string text {get;set;}
-    public ProgramModel program {get;set;}
+    public string text {get;set;}  = null!;
+    public ProgramModel program {get;set;}  = null!;
 
     public UserModel author {get; set;}
 
-    public ImageModel image {get; set;}
+    public ImageModel? image {get; set;}
 
     public DateTime date {get; set;}
 }
