cd021d22ab0aac96016190db6b6813e571dcab96
AlexyaOliveiraIT17 <alexya.oliveira@dellteam.com>
2023-01-24 16:07:47 -0300

    Add editEdition

--- ConnectDellFront/src/components/EditEdition.vue
+++ ConnectDellFront/src/components/EditEdition.vue
@@ -4,4 +4,10 @@
     OU NA MESMA ESTRUTURA DE CREATE PROGRAM??
 
 
+<button @click="$set(edition, 'edit', !edition.edit)">
+  <span v-if ="!edition.edit">Edit</span>
+  <span v-if="edition.edit">Save</span>
+</button>
+
+Source:https://designkojo.com/make-items-editable-vuejs-using-v-if-v-model-directives
 -->
