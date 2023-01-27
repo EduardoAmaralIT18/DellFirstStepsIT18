f0f6f134f64f9c51abadeb14d9f926accfeea9da
JordanaSantosIT17 <jordana.santos@dellteam.com>
2023-01-27 09:36:33 -0300

    resolve merge conflicts

--- ConnectDellFront/src/views/EditProgramView.vue
+++ ConnectDellFront/src/views/EditProgramView.vue
@@ -3,11 +3,6 @@ import EditProgram from '../components/EditProgram.vue';
 import NavBar from '../components/NavBar.vue';
 import SideBar from '../components/SideBar.vue';
 import { RouterView } from 'vue-router';
-<<<<<<< HEAD
-//import AddNews from '@/components/EditProgram.vue';
-import EditProgram from '@/components/EditProgram.vue';
-=======
->>>>>>> 8ed4ca8ea0e580bb69561801b99c44c937c23304
 
 </script>
 <template>
