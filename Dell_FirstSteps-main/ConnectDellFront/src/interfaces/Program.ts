9a3d5fa7a915b42741da6e7de11816b8d76c0cbc
F-MED <128859134+F-MED@users.noreply.github.com>
2023-07-20 15:47:57 -0300

    S1US8: create branch

--- Dell_FirstSteps-main/ConnectDellFront/src/interfaces/Program.ts
+++ Dell_FirstSteps-main/ConnectDellFront/src/interfaces/Program.ts
@@ -1,7 +1,11 @@
+import type Edition from "@/interfaces/Edition";
+
 export default interface Program{
     id?: number;
     name: string;
     description: string;
     startDate?: Date;
     endDate?: Date;
+    editions?: Edition[];
+    owner: string;
 }
---
+++ Dell_FirstSteps-main/ConnectDellFront/src/views/ProgramView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import axios from "axios";
+
+const getProgram = async (programId: number, userId: number) =>{
+  await axios
+      .get(`https://localhost:5001/Program/showInfoProgram?id=${programId}&userId=${userId}`)
+      .then((response) =>{
+
+      }).catch((e) => {
+        console.error(e);
+      })
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
