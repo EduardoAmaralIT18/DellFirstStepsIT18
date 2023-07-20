9783b1559394a59b9eb770bc16ea2ef1976485cc
GuilhermeVasquezIT18 <guilherme.malgarizivasquez@dellteam.com>
2023-07-20 19:32:43 -0300

    S1US5: TextInput component + emit

--- Dell_FirstSteps-main/ConnectDellFront/src/components/ProgramForm.vue
+++ Dell_FirstSteps-main/ConnectDellFront/src/components/ProgramForm.vue
@@ -1,24 +1,14 @@
 <script setup lang="ts">
 import "@dds/components/src/scss/dds-fonts.scss";
+import "@dds/components/src/scss/dds-icons.scss";
 import Dropdown from "@/components/Dropdown.vue"
-
+import TextInput from "./TextInput.vue";
 </script>
 
 <template>
     <div class="form">
         <h2 class="title">Create Program</h2>
-        <div id="text-input-216225955" class="dds__input-text__container">
-            <label id="text-input-label-647192958" for="text-input-control-647192958"
-                class="dds__label dds__label--required">Program Name</label>
-            <div class="dds__input-text__wrapper">
-                <input type="text" class="dds__input-text" name="text-input-control-name-647192958"
-                    id="text-input-control-647192958" required="true"
-                    aria-labelledby="text-input-label-647192958 text-input-helper-647192958" v-model="programInfo.name"
-                    @input="handleInput" />
-                <small id="text-input-helper-647192958" class="dds__input-text__helper"></small>
-                <div id="text-input-error-647192958" class="dds__invalid-feedback">Error text</div>
-            </div>
-        </div>
+        <TextInput boxName="Program Name" @typedText="handleInput"></TextInput>
         <div class="date-container">
             <div class="dds__date-picker" data-dds="date-picker" id="startPicker">
                 <label id="date-picker-label-206993451" for="date-picker-control-name-206993451"
@@ -27,7 +17,7 @@ import Dropdown from "@/components/Dropdown.vue"
                     <input type="date" class="dds__date-picker__input" placeholder="Enter the date" required="true"
                         maxlength="256" id="date-picker-control-206993451" name="date-picker-control-name-206993451"
                         aria-labelledby="date-picker-label-206993451 date-picker-helper-206993451"
-                        v-model="programInfo.startDate" @input="handleInput" />
+                        v-model="programInfo.startDate" @input="" />
                     <small id="date-picker-helper-206993451" class="dds__date-picker__helper">Please, use the format
                         MM/DD/YYYY</small>
                     <div id="date-picker-error-206993451" class="dds__date-picker__invalid-feedback">Invalid date</div>
@@ -59,7 +49,7 @@ import Dropdown from "@/components/Dropdown.vue"
             <div class="dds__text-area__wrapper">
                 <textarea class="dds__text-area" name="text-area-control-name-867784391" id="text-area-control-867784391"
                     aria-required="true" aria-labelledby="text-area-label-867784391 text-area-helper-867784391" required
-                    v-model="programInfo.description" @input="handleInput" minlength="10" maxlength="50"></textarea>
+                    v-model="programInfo.description" @input="" minlength="10" maxlength="50"></textarea>
                 <small id="text-area-helper-359524269" class="dds__input-text__helper">Helper Text</small>
                 <small id="text-area-error-867784391" class="dds__invalid-feedback">Error text</small>
             </div>
@@ -70,9 +60,7 @@ import Dropdown from "@/components/Dropdown.vue"
 
 <script lang="ts">
 import Program from "@/interfaces/Program";
-import User from "@/interfaces/User";
 import axios from "axios";
-import { onMounted } from "vue";
 
 export default {
     data() {
@@ -88,7 +76,8 @@ export default {
         };
     },
     methods: {
-        handleInput() {
+        handleInput(text: string) {
+            this.programInfo.name = text;
             console.log(this.programInfo.name);
             console.log(this.programInfo.description);
             console.log(this.programInfo.startDate);
--- Dell_FirstSteps-main/ConnectDellFront/src/components/TextInput.vue
+++ Dell_FirstSteps-main/ConnectDellFront/src/components/TextInput.vue
@@ -1,33 +1,36 @@
-<script setup>
-import "@dds/components/src/scss/dds-fonts.scss";
-import { ref, defineProps, defineEmits } from 'vue';
-
-const props = defineProps(['value']); 
-
-const inputValue = ref(props.value)
-
-const { emit } = defineEmits()
-
-const handleInput = (event) => { 
-    inputValue.value = event.target.value
-    emit('input', inputValue.value)
-};
-
-</script>
-
 <template>
     <div id="text-input-216225955" class="dds__input-text__container">
-        <label id="text-input-label-647192958" for="text-input-control-647192958" class="dds__label dds__label--required">{{
-            boxName }}</label>
+        <label id="text-input-label-647192958" for="text-input-control-647192958" class="dds__label dds__label--required">{{ boxName }}</label>
         <div class="dds__input-text__wrapper">
             <input type="text" class="dds__input-text" name="text-input-control-name-647192958"
                 id="text-input-control-647192958" required="true"
-                aria-labelledby="text-input-label-647192958 text-input-helper-647192958"/>
+                aria-labelledby="text-input-label-647192958 text-input-helper-647192958" v-model="info" @input="sendTextToParent(info)" />
             <small id="text-input-helper-647192958" class="dds__input-text__helper"></small>
             <div id="text-input-error-647192958" class="dds__invalid-feedback">Error text</div>
         </div>
     </div>
 </template>
 
+<script lang="ts">
+import "@dds/components/src/scss/dds-fonts.scss";
+
+export default {
+    name: "Dropdown",
+    data() {
+        return {
+            info: "",
+        };
+    },
+    props: {
+        boxName: String
+    },
+    methods: {
+        sendTextToParent (text: string) {
+            this.info = text;
+            this.$emit("typedText", text);
+        },
+    }
+};
+</script>
 
 <style scoped></style>
