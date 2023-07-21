9ce0359d4a5e6a41eb7c8dae4129f3c85f9f0958
EduardoAmaralIT18 <eduardo.lopesdoamaral@dellteam.com>
2023-07-21 17:02:53 -0300

    S1US4: users page and some methods

--- Dell_FirstSteps-main/ConnectDellFront/src/router/index.ts
+++ Dell_FirstSteps-main/ConnectDellFront/src/router/index.ts
@@ -1,28 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory} from 'vue-router'
 import MyProgramView from "@/views/MyProgramView.vue";
 import ProgramView from "@/views/ProgramView.vue";
+import UsersView from "@/views/UsersView.vue";
+import HomeView from "@/views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/Home',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/myProgram/:id',
-      name: 'My Program',
-      component: MyProgramView
-    },
-    {
-      path: '/program/:id',
-      name: 'Program',
-      component: ProgramView
-    }
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/myProgram/:id',
+            name: 'My Program',
+            component: MyProgramView
+        },
+        {
+            path: '/program/:id',
+            name: 'Program',
+            component: ProgramView
+        }, {
+            path: '/users',
+            name: 'Users',
+            component: UsersView
+        }
 
-  ]
+    ]
 })
 
 export default router
