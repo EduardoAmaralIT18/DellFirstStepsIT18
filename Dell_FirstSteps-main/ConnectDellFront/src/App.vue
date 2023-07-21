157cfec16916675cf1819f77efe70d00c133f70c
LuanaFariasDaSilvaIT18 <Luana.FariasDaSilva@dellteam.com>
2023-07-21 17:26:36 -0300

    Header refactor

--- Dell_FirstSteps-main/ConnectDellFront/src/App.vue
+++ Dell_FirstSteps-main/ConnectDellFront/src/App.vue
@@ -4,48 +4,47 @@ import "@dds/components/src/scss/dds-fonts.scss";
 import "@dds/components/src/scss/dds-icons.scss";
 import "@dds/components/src/scss/dds-helpers.scss";
 import "@dds/components/src/scss/dds-main.scss";
-import Header from "./components/Header.vue"
+import Header from "./components/Header.vue";
 import Sidebar from "./components/Sidebar.vue";
-import { ref, watch } from 'vue';
+import { ref, watch } from "vue";
 import { useRoute } from "vue-router";
 
-localStorage.setItem("name", '');
-localStorage.setItem("id", '');
-localStorage.setItem("email", '');
-localStorage.setItem("role", '');
-
+localStorage.setItem("name", "");
+localStorage.setItem("id", "");
+localStorage.setItem("email", "");
+localStorage.setItem("role", "");
 
 const name = ref();
 const role = ref();
 
-const route = useRoute()
-watch(route,()=>{
-    name.value = localStorage.getItem("name");
-    role.value = localStorage.getItem("role");
-})
+const route = useRoute();
+watch(route, () => {
+  name.value = localStorage.getItem("name");
+  role.value = localStorage.getItem("role");
+});
 </script>
 
 <template class="template">
-    <Header class="header" :name="name" :role="role"/>
-    <div class="container">
-    <Sidebar class="sidebar" v-if="role !== ''"/>
-        <main class="main" >
-            <RouterView id="router"/>
-        </main>
-    </div>
+  <Header class="header" :name="name" :role="role" />
+  <div class="container">
+    <Sidebar class="sidebar" v-if="role !== ''" />
+    <main class="main">
+      <RouterView id="router" />
+    </main>
+  </div>
 </template>
 
 <style scoped>
-    .template{
-        height: 100vh;
-    }
-    .container {
-        height: 92vh; /* TO-DO */
-        display: flex;
-        flex-direction: row; 
-    }
+.template {
+  height: 100vh;
+}
+.container {
+  height: 92vh; /* TO-DO */
+  display: flex;
+  flex-direction: row;
+}
 
-    .main {
-        flex-grow: 2;
-    }
+.main {
+  flex-grow: 2;
+}
 </style>
--- Dell_FirstSteps-main/ConnectDellFront/src/components/Header.vue
+++ Dell_FirstSteps-main/ConnectDellFront/src/components/Header.vue
@@ -1,54 +1,38 @@
 <script setup lang="ts">
-import { computed } from "vue";
+defineProps({
+  name: String,
+  role: Number,
+});
 
-const prop = defineProps({
-  name:String,
-  role:String
-})
-
-
-const showRole = computed(() =>{
-  if (prop.role === '0') {
-    return "Admin";
-  } else if (prop.role === '1') {
-    return "Intern";
-  } else if (prop.role === '2') {
-    return "Dell Manager";
-  } else if (prop.role === '3') {
-    return "Dell Member";
-  } else if (prop.role === '4') {
-    return "Dell Pucrs Staff";
-  }
-  return "-1";
-})
+const type: any = {
+  0: "Admin",
+  1: "Intern",
+  2: "Dell Manager",
+  3: "Dell Member",
+  4: "Dell Pucrs Staff",
+  5: "",
+};
 </script>
 
 <template>
   <header class="navbar shadow-lg fixed-top">
-      <div class="container-fluid">
-          <div class="row">
-              <div class="col-4 dellLogo">
-                <a><img class="logo" alt="logo Dell" src="../assets/logoDell.png"></a>
-              </div>
-              <div v-if=" showRole !== '-1'" class="col-8">
-              <nav class="userName">
-                  <div class="d-none d-lg-block">
-                    <p class="name"> {{ name }} | {{ showRole }}<img class="userPicture" alt="user icon" src="../assets/user.png"></p>
-                  </div>
-                </nav>            
-              </div>
-
-          </div>
-      </div>
+    <img class="logo" alt="logo Dell" src="../assets/logoDell.png" />
+    <p v-if="name && role" class="name">{{ name }} | {{ type[role] }}</p>
+    <img
+      v-if="name && role"
+      class="userPicture"
+      alt="icon"
+      src="../assets/user.png"
+    />
   </header>
 </template>
 
 <style>
 .navbar {
-  padding-bottom: 0;
   display: flex;
   vertical-align: bottom;
-  line-height: 50px;
+
+  align-items: center;
   background-color: white;
   width: 100%;
   border-bottom: 2px solid #a6a6a6;
@@ -56,30 +40,16 @@ const showRole = computed(() =>{
 }
 .name {
   color: #0672cb;
-  font-size: 151%;
-  position: absolute;
-  right: 0;
-  top: 0;
-  margin-top: 10px;
-  margin-right: 1%;
+  font-size: 145%;
+  margin-top: 0.8%;
+  margin-left: auto;
 }
 .userPicture {
   width: 47px;
-  margin-left: 1px;
-  margin-bottom: 3%;
-  padding-top: 4px;
 }
-.icon {
-  font-size: 13em;
-  background-color: #0672cb;
-  margin-left: 3%;
-  padding: 10;
-  position: relative;
-  line-height: 0.2em;
-}
-.logo{
-  height: 55px;
-  margin-top: 10px;
+.logo {
+  height: 45px;
+
   margin-left: 1%;
 }
 </style>
