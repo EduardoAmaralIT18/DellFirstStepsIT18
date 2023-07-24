5e780d74a6069a91213eba3259a5d7d5683864ee
EduardoAmaralIT18 <eduardo.lopesdoamaral@dellteam.com>
2023-07-24 11:57:07 -0300

    S1US4: rebase -> dev

--- Dell_FirstSteps-main/ConnectDellFront/src/views/UsersView.vue
+++ Dell_FirstSteps-main/ConnectDellFront/src/views/UsersView.vue
@@ -1,11 +1,101 @@
 <script setup lang="ts">
 
+import {onMounted, ref} from "vue";
+import axios from "axios";
+import type User from "@/interfaces/User";
+import router from '@/router';
+
+const users = ref<User[]>()
+onMounted(async () => {
+  redirectIfNotAdmin();
+  await findAllUsers();
+})
+
+
+const deleteUser = async (id: number) => {
+  await axios.delete(`https://localhost:5001/User/removeUser/${id}`)
+      .catch((e) => {
+        console.error(e);
+      })
+}
+const findAllUsers = async () => {
+  await axios.get('https://localhost:5001/User/listUsers')
+      .then((response) => {
+        users.value = response.data
+      }).catch((e) => {
+        console.error(e);
+      })
+}
+
+const itsMe = (id: number) => {
+  return id == +localStorage.getItem('userId');
+}
+
+const redirectIfNotAdmin = () => {
+  if (localStorage.getItem('userRole') != 0) {
+    router.push('/');
+  }
+}
+
+const roles = [
+  "Admin",
+  "Intern",
+  "Dell Manager",
+  "Dell Member",
+  "Puc-RS Staff"
+]
 </script>
 
 <template>
+  <div class="container">
+    <table id="components-table--static" class="dds__table">
+      <thead class="dds__thead">
+      <tr class="dds__tr">
+        <th class="dds__th">Nome</th>
+        <th class="dds__th">Email</th>
+        <th class="dds__th">Cargo</th>
+        <th id="delete-th" class="dds__th"><i class="dds__icon dds__icon--gear" aria-hidden="true"></i></th>
+      </tr>
+      </thead>
+      <tbody class="dds__tbody">
+      <tr class="dds__tr" v-for="user in users">
+        <td class="dds__td">{{ user.name }}</td>
+        <td id="email_td" class="dds__td">{{ user.email }}</td>
+        <td class="dds__td">{{ roles[user.role] }}</td>
+        <td class="dds__td" id="delete-td">
+          <button v-if="!itsMe(user.id)" class="red dds__button dds__button--destructive" :id="user.id" type="button" @click="deleteUser(user.id)">
+            <i class="dds__icon dds__icon--user-remove" aria-hidden="true"></i>
+          </button>
+        </td>
+      </tr>
+      </tbody>
 
+    </table>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  padding: 5% 20% 50vh 20%;
+  display: flex;
+  flex-direction: column;
+  background-color: #efefef;
+}
 
+td {
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+  word-break: break-all;
+  height: 70px;
+  padding: 0;
+}
+
+table.dds__table {
+  display: table;
+}
+
+button.red {
+  width: 25%;
+  height: 70%;
+}
 </style>
