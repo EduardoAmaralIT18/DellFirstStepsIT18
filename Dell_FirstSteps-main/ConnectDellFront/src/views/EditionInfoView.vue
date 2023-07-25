225f2d8bec57751e66e0b0230f548ad852dc66f2
MatheusOliveiraIT18 <matheus.buenodeoliveira@dellteam.com>
2023-07-25 13:18:59 -0300

    S1US14 altering users in dropdown

--- Dell_FirstSteps-main/ConnectDellFront/src/views/EditionInfoView.vue
+++ Dell_FirstSteps-main/ConnectDellFront/src/views/EditionInfoView.vue
@@ -1,12 +1,14 @@
 <template>
-<ModalForm modalTitle="Add Event" :editionUsers="data"></ModalForm>
+<ModalForm modalTitle="Add Event" :editionUsers="data.members"></ModalForm>
 </template>
 
 <script setup lang="ts">
 import ModalForm from '@/components/ModalForm.vue';
 import axios from 'axios';
 import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
 const data= ref([]);
+
 async function handleClick() {
             await axios
                 .get('https://localhost:5001/edition/getUsersNotAdmin')
@@ -17,8 +19,24 @@ async function handleClick() {
                 alert("Não foi possível atender a solicitação.");
             });
         }
+        async function getMembers(){
+            await axios .get(`https://localhost:5001/edition/showInfoEdition?idProgram=0&idEdition=${id}`)
+            .then((response) => {console.log(response.data)
+                data.value=response.data
+            })
+            .catch((error) => {
+                alert("Não foi possível atender a solicitação.");
+            });
+            
+
+        }
 onMounted(async() => {
-    await handleClick();
+    await getMembers();
+    console.log(data.memberships);
+
 })
+
+const route = useRoute();
+const id = +route.params.id;
 
 </script>
